9838d59453ab904d379be9ad632074ebbb847bf9
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-11-11 15:24:41 +0300

    camera1

--- DURACHOK/Assets/Scripts/Camera.cs
+++ DURACHOK/Assets/Scripts/Camera.cs
@@ -10,18 +10,14 @@ public class IsometricCamera : MonoBehaviour
 
     private void Start()
     {
-        // —охран€ем начальную позицию камеры
         initialCameraPosition = transform.position - player.position;
     }
 
     private void LateUpdate()
     {
         if (player == null) return;
-
-        //  амера будет следовать за игроком, сохран€€ начальное смещение
         Vector3 targetPosition = player.position + initialCameraPosition;
 
-        // ѕровер€ем, отличаетс€ ли нова€ позици€ камеры от текущей
         if ((targetPosition - transform.position).sqrMagnitude > 0.001f)
         {
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
